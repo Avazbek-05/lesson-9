0ed27d922dd6db13d9f514c84000a9f1d92da3c4
Avaz <avazbekrozimboyev08@gmail.com>
2025-05-01 00:50:09 +0500

    dfgh

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -9,28 +9,35 @@ const Card = () => {
   });
   return (
     <section className="my-10">
-      <div className="container1 grid gap-5  grid-cols-5 m-auto">
+      <div className="container1 grid gap-5  grid-cols-5 m-auto max-[1100px]:grid-cols-4 max-[950px]:grid-cols-3 max-[700px]:grid-cols-2 max-[380px]:grid-cols-1">
         {data.map((value) => (
-          <div key={value.id} className=" ">
+          <div key={value.id} className="flex flex-col max-[380px]:items-center max-[380px]:justify-center max-[380px]:text-center">
             <div className="flex flex-col border rounded-lg border-[#f4f6f7]   items-center justify-center">
               <img
-                className="w-[200px] p-4 h-[200px]"
+                className="w-[200px] p-1 h-[200px]"
                 src={value.img}
                 alt="{value.title}"
               />
             </div>
-            <div>
-              <h1 className="text-sm  text-gray-900">{value.title}</h1>
-              <div className="cursor-pointer border border-[#ffca48] bg-[#ffedc2] py-1 px-2  w-fit rounded-2xl text-sm">
+            <div className="flex flex-col gap-1 mt-2">
+              <h1 className="text-sm  text-gray-900">
+                {value.title.slice(0, 50) + "..."}
+              </h1>
+              <div className="cursor-pointer border border-[#ffca48] bg-[#ffedc2] py-1 px-2  w-fit rounded-2xl text-sm max-[380px]:w-full">
                 {"от " + value.moth.toLocaleString("ru-RU") + " сум/мес"}
               </div>
               <p className="text-gray-300 text-lg  font-normal line-through">
                 {value.price.toLocaleString("ru-RU") + " сум"}
               </p>
-              <p className="text-[red]">{value.oldPrice.toLocaleString("ru-RU") +" сум" }</p>
-              <button className="flex items-center justify-between w-fit p-2 bg-[#ffbe1e]">
-              <ShoppingCart />
-              <p> В корзину</p>
+              <p className="text-[red]">
+                {value.oldPrice.toLocaleString("ru-RU") + " сум"}
+              </p>
+              <button className="relative overflow-hidden cursor-pointer flex items-center rounded-lg gap-5 justify-between w-fit p-2 bg-[#ffbe1e] group max-[380px]:w-full  max-[380px]:justify-center" >
+                <span className="absolute inset-0 bg-yellow-400 w-0 group-hover:w-full transition-all duration-100 ease-in-out z-0"></span>
+                <div className="flex items-center gap-2 z-10 text-black">
+                  <ShoppingCart />
+                  <p>В корзину</p>
+                </div>
               </button>
             </div>
           </div>
---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,117 @@
+import React from "react";
+import Instagram from "../../assets/imgs/instagram.webp";
+import Facebook from "../../assets/svg/facebook.svg";
+import Telegram from "../../assets/svg/telegram.svg";
+import Ok from "../../assets/svg/ok.svg";
+import Tiktok from "../../assets/svg/tiktok.svg";
+const Footer = () => {
+  return (
+    <footer className="bg-[#25272c]">
+      <div className="container1 py-6">
+        <div className="grid grid-cols-4 gap-5">
+          <div className="flex flex-col gap-3">
+            <a className="text-gray-500" href="#">
+              Документы
+            </a>
+            <a
+              className="text-white hover:text-[#ffbe1e] transition-all duration-300"
+              href="#"
+            >
+              Общие условия продажи
+            </a>
+            <a
+              className="text-white hover:text-[#ffbe1e] transition-all duration-300"
+              href="#"
+            >
+              Устав
+            </a>
+            <a
+              className="text-white hover:text-[#ffbe1e] transition-all duration-300"
+              href="#"
+            >
+              Свидетельство
+            </a>
+          </div>
+          <div className="flex flex-col gap-3">
+            <a className="text-gray-500" href="#">
+              Сервис
+            </a>
+            <a
+              className="text-white hover:text-[#ffbe1e] transition-all duration-300"
+              href="#"
+            >
+              Время намаза
+            </a>
+            <a
+              className="text-white hover:text-[#ffbe1e] transition-all duration-300"
+              href="#"
+            >
+              Рассрочка в Исламе
+            </a>
+            <a
+              className="text-white hover:text-[#ffbe1e] transition-all duration-300"
+              href="#"
+            >
+              Продавайте на alif shop!
+            </a>
+            <a
+              className="text-white hover:text-[#ffbe1e] transition-all duration-300"
+              href="#"
+            >
+              Возвраты
+            </a>
+          </div>
+          <div className="flex flex-col gap-3">
+            <a className="text-gray-500" href="#">
+              Каталог товаров
+            </a>
+            <a
+              className="text-white hover:text-[#ffbe1e] transition-all duration-300"
+              href="#"
+            >
+              Смартфоны и телефоны{" "}
+            </a>
+            <a
+              className="text-white hover:text-[#ffbe1e] transition-all duration-300"
+              href="#"
+            >
+              Гаджеты
+            </a>
+            <a
+              className="text-white hover:text-[#ffbe1e] transition-all duration-300"
+              href="#"
+            >
+              Аксессуары для телефонов
+            </a>
+            <a
+              className="text-white hover:text-[#ffbe1e] transition-all duration-300"
+              href="#"
+            >
+              Часы и аксессуары
+            </a>
+            <a
+              className="text-white hover:text-[#ffbe1e] transition-all duration-300"
+              href="#"
+            >
+              Cопутствующие товары
+            </a>
+          </div>
+          <div className="flex flex-col gap-3">
+            <a className="text-gray-500" href="#">
+              Документы
+            </a>
+            <div className="flex items-center gap-2">
+              <img className="cursor-pointer" src={Instagram} alt="Instagram" />
+              <img className="cursor-pointer" src={Facebook} alt="Facebook" />
+              <img className="cursor-pointer" src={Telegram} alt="Telegram" />
+              <img className="cursor-pointer" src={Ok} alt="Telegram" />
+              <img className="cursor-pointer" src={Tiktok} alt="Telegram" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
